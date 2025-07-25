7473d2ccfb16a5c63824e8b3ae2546d9e700a930
Your Name <you@example.com>
2025-07-25 17:30:29 +0100

    commit

---
+++ num.py
@@ -0,0 +1,8 @@
+#Write a program to check whether the given number is greater than 15 or smaller than 15.
+
+num=int(input("Enter any number to check if it is greater than 15 or smaller than 15: "))
+if num > 15:
+
+    print(f"{num}>15")
+else:
+    print(f"{num}<15")
