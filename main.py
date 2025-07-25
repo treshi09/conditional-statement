cc98122181eb1d2e69962dcde4e4e59d93f84cb0
Your Name <you@example.com>
2025-07-25 17:13:54 +0100

    commit

---
+++ main.py
@@ -0,0 +1,7 @@
+#write a program to check whether the given number is positive?
+
+num=int(input("Please enter a number: "))
+if num > 0:
+    print("The number is positive.")
+else:
+    print("The number is negative.")
